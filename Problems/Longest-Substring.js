116bf5d18ab93e3cae6cdb70b327100cd466e500
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-06-09 17:14:06 +0300

    Update Solution

--- Problems/Longest-Substring.js
+++ Problems/Longest-Substring.js
@@ -1,20 +1,23 @@
 var lengthOfLongestSubstring = function(s) {
     let result = '';
-    let map = new Map();
 
     if (s.length < 2) {
         return s.length;
     }
 
+    let lastIndex = 0;
+    let character = s[0];
+
     for (let index = 0; index < s.length; index++) {
-        let character = s.charAt(index)
-        if (character != map.keys()[index + 1] && !map.has(character)) {
-            result += character;
+
+        if (character != s[index] && !result.includes(character)) {
+            result = s.substr(lastIndex, index);
+        } else {
+            lastIndex = index;
         }
 
-        map.set(character, index);
+        character = s[index];
     }
-
     console.log(result);
     console.log(result.length);
     return result.length;
@@ -25,3 +28,5 @@ lengthOfLongestSubstring(
 
 lengthOfLongestSubstring(
     "abcabcbb");
+
+lengthOfLongestSubstring("bbbbb");
