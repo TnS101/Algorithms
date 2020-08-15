cdcca38b751ee4bcb222456cfb6c1309d516a4ab
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-15 13:24:25 +0300

    Add Solutions

---
+++ Problems/C#/Fundaments/NumericOperations.cs
@@ -0,0 +1,48 @@
+﻿namespace Interview_Prep.Fundaments
+{
+    using System;
+
+    public class NumericOperations
+    {
+        public void SwapNumberValues(int num1, int num2) 
+        {
+            num1 = num2 ^ num1;
+            num2 = num2 ^ num1;
+            num1 = num1 ^ num2;
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+
+        public bool IsArmstrongNumber(int num) 
+        {
+            double sum = 0;
+            var asString = num.ToString().ToCharArray();
+
+            for (int i = 0; i < asString.Length; i++)
+            {
+                sum += Math.Pow(int.Parse(asString[i].ToString()), asString.Length);
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(num);
+
+            if (sum == num)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public double GetRemainder(int num1, int num2) 
+        {
+            return num1 % num2;
+        }
+
+        public bool IsNumberDivisibleByTwo(int num) 
+        {
+            return num % 2 == 0;
+        }
+    }
+}
--- Problems/C#/Program.cs
+++ Problems/C#/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace Interview_Prep
 {
+    using Interview_Prep.Fundaments;
     using Interview_Prep.Problems;
     using System;
     using System.Diagnostics;
@@ -9,25 +10,8 @@
     {
         static void Main(string[] args)
         {
-            var sw = new Stopwatch();
+            var numeric = new NumericOperations();
 
-            var arr1 = new int[10000];
-            var arr2 = new int[10000];
-
-            for (int i = 0; i < arr1.Length; i++)
-            {
-                arr1[i] = new Random(i + 89890).Next(0, 1001);
-                arr2[i] = new Random(i + 500).Next(0, 1001);
-            }
-
-            Array.Sort(arr1);
-            Array.Sort(arr2);
-
-            sw.Start();
-            new ArrayOperations().MergeTwoSortedArrays(arr1, arr2);
-            sw.Stop();
-
-            Console.WriteLine(sw.ElapsedMilliseconds);
         }
     }
 
