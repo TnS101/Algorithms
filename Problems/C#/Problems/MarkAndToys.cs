a634e97f8081298b8f1a68389d4c4e3ae04e41d9
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-28 20:47:08 +0300

    Add Solution

---
+++ Problems/C#/Problems/MarkAndToys.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview_Prep.Problems
+{
+    public class MarkAndToys
+    {
+        public void Exe(int money, int[] arr) 
+        {
+            var temp = 0;
+
+            var result = 0;
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                Console.WriteLine($"{temp} -> {arr[i]}");
+                if (temp == 0)
+                {
+                    if (arr[i] < arr[arr.Length - i - 1])
+                    {
+                        temp = arr[arr.Length - i - 1];
+                        if (money - arr[i] >= 0)
+                        {
+                            money -= arr[i];
+                            result++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (arr[i] < temp)
+                    {
+                        if (money - arr[i] >= 0)
+                        {
+                            money -= arr[i];
+                            result++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (money - temp >= 0)
+                        {
+                            money -= temp;
+                            result++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                        temp = arr[i];
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
