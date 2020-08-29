82c2d8d397479b119ceb9dfca1e756dfb792a585
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-29 10:09:59 +0300

    Add Solutions

---
+++ Problems/C#/Problems/JumpingOnClouds.cs
@@ -0,0 +1,40 @@
+﻿namespace Interview_Prep.Problems
+{
+    public class JumpingOnClouds
+    {
+        public int Exe(int n, int[] arr)
+        {
+            var result = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i + 1 < arr.Length && i + 2 < arr.Length)
+                {
+                    if (arr[i + 2] == 0 && i + 4 < arr.Length && arr[i + 4] == 0)
+                    {
+                        result++;
+                        i++;
+                        continue;
+                    }
+
+                    if (arr[i + 1] == 0)
+                    {
+                        result++;
+                        continue;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+
+                if (arr[i] == 0)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Problems/C#/Problems/RepeatedString.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interview_Prep.Problems
+{
+    public class RepeatedString
+    {
+        public long Exe(string s, long n) 
+        {
+            if (s.Length == 1)
+            {
+                return n;
+            }
+
+            var occurence = 0;
+
+            var result = 0;
+            var arr = s.ToCharArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 'a')
+                {
+                    occurence++;
+                }
+            }
+
+            if (n % s.Length == 0)
+            {
+                result += (int)((occurence * n) / s.Length);
+            }
+            else
+            {
+                var remainder = n % s.Length;
+                var whole = (n - remainder) / s.Length;
+
+                result += (int)whole * occurence;
+
+                var length = (int)remainder;
+
+
+                for (int i = 0; i < length; i++)
+                {
+                    if (arr[i] == 'a')
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
