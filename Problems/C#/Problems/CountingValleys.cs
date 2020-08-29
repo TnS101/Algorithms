cb9152f360d8eeee74753b666127355a17447d5d
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-29 07:53:06 +0300

    Add Solutions

---
+++ Problems/C#/Problems/CountingValleys.cs
@@ -0,0 +1,34 @@
+﻿namespace Interview_Prep.Problems
+{
+    public class CountingValleys
+    {
+        public int Exe(int n, string s)
+        {
+            var result = 0;
+            var counter = 0;
+            var arr = s.ToCharArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (s[i] == 'U')
+                {
+                    counter++;
+                }
+                else if (s[i] == 'D')
+                {
+                    counter--;
+                }
+
+                if (counter == 0)
+                {
+                    if (s[i] != 'D')
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Problems/C#/Problems/SockMerchant.cs
@@ -0,0 +1,25 @@
+﻿namespace Interview_Prep.Problems
+{
+    using System;
+
+    public class SockMerchant
+    {
+        public int Exe(int n, int[] arr) 
+        {
+            Array.Sort(arr);
+
+            var result = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] == arr[i])
+                {
+                    result++;
+                    i++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
