854d4d64f7c7bf51ad273602faeace51129d0bf9
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-30 14:26:28 +0300

    Add Solutions

---
+++ Problems/C#/Problems/BubbleSortCounter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Interview_Prep.Problems
+{
+    public class BubbleSortCounter
+    {
+        public void Exe(int[] arr)
+        {
+            var result = 0;
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                if (arr[i] > arr[arr.Length - i - 1])
+                {
+                    var temp = arr[i];
+                    arr[i] = arr[arr.Length - i - 1];
+                    arr[arr.Length - i - 1] = temp;
+                    result++;
+                }
+            }
+
+            if (result == 0)
+            {
+                Print(result, arr);
+
+                return;
+            }
+
+            var swapped = result * 2;
+
+            if (arr.Length % 2 == 0)
+            {
+                result += swapped + arr.Length - swapped;
+            }
+            else
+            {
+                result = swapped + arr.Length - swapped;
+            }
+
+
+            Print(result, arr);
+
+            return;
+        }
+
+        private void Print(int result, int[] arr) 
+        {
+            Console.WriteLine($"Array is sorted in {result} swaps.");
+            Console.WriteLine($"First Element: {arr[0]}");
+            Console.WriteLine($"Last Element: {arr[arr.Length - 1]}");
+        }
+    }
+}
---
+++ Problems/C#/Problems/CountrTripplets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Interview_Prep.Problems
+{
+    public class CountrTripplets
+    {
+        public long Exe(List<long> arr)
+        {
+            if (arr.Count < 3)
+            {
+                return 0;
+            }
+
+            var set = new HashSet<long>(arr);
+            var right = 2 * (arr.Count - set.Count);
+
+            return arr.Count - 2 * right + arr.Count - right;
+        }
+    }
+}
