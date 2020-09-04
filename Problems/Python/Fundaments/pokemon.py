3c2c426ca388ba9b855567dfe824256a431d31fb
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-09-04 15:51:03 +0300

    Add Pokemon Mini-Game

---
+++ Problems/Python/Fundaments/pokemon.py
@@ -0,0 +1,105 @@
+class Ability:
+    def __init__(self, name, aType, element, power, operator, energy):
+        self.name = name
+        self.aType = aType
+        self.element = element
+        self.power = power
+        self.operator = operator
+        self.energy = energy
+    
+    def use(self, pokemon):
+        self.energy -= 1
+        value = pokemon.__getattribute__(self.aType.lower())
+
+        if self.operator == '+':
+            pokemon.__setattr__(self.aType.lower(), value + self.power)
+        else:
+            pokemon.__setattr__(self.aType.lower(), value - self.power)
+        
+        message = '{} was used on {}'
+        print(message.format(self.name, pokemon.name))
+     
+
+class Pokemon:
+    def __init__(self, name, element, hunger, thirst, attack, health, abilities):
+        self.name = name
+        self.element = element
+        self.hunger = hunger
+        self.thirst = thirst
+        self.energy = 10
+        self.attack = attack
+        self.health = health
+        self.abilities = abilities
+
+    def feed(self, amount):
+        if self.hunger == 0:
+            print('Pokemon is not Hungry!')
+            return
+
+        self.hunger -= amount
+        message = ''
+        if self.hunger > 0:
+            message = '{} was fed but its still Hungry! Needs {} more food to feel well!'
+            print(message.format(self.name, self.hunger))
+        else:
+            message = '{} was fully fed!'
+            print(message.format(self.name))
+        self.energy += 1
+
+    def info(self):
+        message = '\n-----------\n Name : {}\n Element : {}\n Hunger : {}\n Thirst : {}\n Energy : {}\n Attack : {}\n Health : {}\n-----------\n'
+        print(message.format(self.name, self.element, self.hunger, self.thirst, self.energy, self.attack, self.health))   
+
+    def irrigate(self, amount):
+        if self.thirst == 0:
+            print('Pokemon is not Thirsty!')
+            return
+
+        self.thirst -= amount
+        message = ''
+        if self.thirst > 0:
+            message = '{} was irrigated but its still Thirsty! Needs {} more liquids to feel well!'
+            print(message.format(self.name, self.thirst))
+        else:
+            message = '{} was fully irrigated!'
+            print(message.format(self.name))
+        self.energy += 1
+
+
+    def basicAttack(self, pokemon):
+        if self.hunger > 5:
+            print('Pokemon is Hungry!')
+            return
+        if self.thirst > 5:
+            print('Pokemon is Thirsty!')
+            return
+
+        pokemon.health -= self.attack
+        self.energy -= 1
+        self.hunger -= 1
+        self.thirst -= 1
+        message = '{} was hit by {} for {} Damage!'
+        print(message.format(pokemon.name, self.name, self.attack))
+
+    def useAbility(self, pokemon, abilityName):
+        ability = {}
+        for ab in self.abilities:
+            if ab.name == abilityName:
+                ability = ab
+                break
+            else:
+                message = 'Ability with name {} was not found!'
+                raise Exception(message.format(abilityName))
+        self.energy -= 1        
+        ability.use(pokemon)
+     
+
+thunderBolt = Ability('Thunder Bolt', 'Health', 'Thunder', 5, '-', 10)
+pikachu = Pokemon('Pikachu', 'Thunder', 10, 10, 5, 20, [thunderBolt])
+
+peepLup = Pokemon('Peeplup', 'Water', 10, 10, 5, 10, [])
+
+pikachu.useAbility(peepLup, 'Thunder Bolt')
+
+print(peepLup.info())
+print(pikachu.info())
