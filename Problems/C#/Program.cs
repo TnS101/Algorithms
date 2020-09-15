2e6c3e7863b48155fc3b81c6155d709cfd2b4888
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-09-15 17:40:29 +0300

    Update Test Solution

--- Problems/C#/Program.cs
+++ Problems/C#/Program.cs
@@ -8,45 +8,27 @@ namespace Interview_Prep
     {
         static void Main(string[] args)
         {
-            var arr = new List<int>() { 4, 6, 5, 3, 3, 1 };
-            arr = arr.OrderBy(i => i).ToList();
+            var arr = new int[] { 1, 2, 4, 5, 3, 8, -3, -1 };
+            var dict = new Dictionary<int, int>();
 
-            var result = 0;
-            var temp = -1;
-
-            for (int i = 1; i < arr.Count; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                var diff = Math.Abs(arr[i - 1] - arr[i]);
-                if (temp == -1)
+                var num = Math.Abs(arr[i]);
+
+                if (!dict.ContainsKey(num))
                 {
-                    if (diff <= 1)
-                    {
-                        result++;
-                    }
-                    else if (diff > 1 && result > 0)
-                    {
-                        temp = 0;
-                    }
+                    dict.Add(num, i);
                 }
                 else
                 {
-                    if (diff <= 1)
-                    {
-                        temp++;
-                    }
-                    else
-                    {
-                        if (temp > result)
-                        {
-                            result = temp;
-                        }
-
-                        temp = -1;
-                    }
+                    dict[num] += arr.Length;
                 }
             }
 
-            Console.WriteLine(result);
+            foreach (var item in dict.Where(n => n.Value >= arr.Length).OrderBy(n => n.Value))
+            {
+                Console.WriteLine($"{item.Key * -1} -> {item.Key}");
+            }
         }
     }
 
