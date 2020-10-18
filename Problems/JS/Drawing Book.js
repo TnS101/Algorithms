680b1cccc864803ea1db96bae63b76fde164cac2
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-10-18 15:11:31 +0300

    Update Drawing Book.js

--- Problems/JS/Drawing Book.js
+++ Problems/JS/Drawing Book.js
@@ -1,12 +1,20 @@
-function solution(n, target) {
-    var bot = target;
-    var top = n - target;
-
-    if (bot > top) {
-        return top / 2;
+function solution(n, p) {
+    if (n == 2) {
+        return 0;
     }
 
-    return target / 2;
+    if (n - p == 1 && n % 2 == 0) {
+        return 1;
+    }
+
+    var bot = p;
+    var top = n - p;
+
+    if (bot < top) {
+        return p / 2;
+    }
+
+    return top / 2;
 }
 
 console.log(solution(6, 2));
